55099be40253fecde8be9fe667b35efb636dd996
fsd5802 <fsd5802@gmail.com>
2024-11-14 16:20:35 +0200

    Second_Updated

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     <div className = "App" >
         <Router >
           < Navbar />
-    
+     
           <Routes > 
              <Route path="/" element={<Home />} />
              <Route path="/about" element={<About />} />
